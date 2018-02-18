ad1eb7a6912088dc773dade59b43c49b1e4c5656
Michał Błotniak <buoto336@gmail.com>
2018-02-18 01:58:35 +0100

    Add second find difference

--- src/components/FindDifferencePosts.js
+++ src/components/FindDifferencePosts.js
@@ -8,15 +8,8 @@ export const postsByVisibility = visible => [
       heading: <div>NIESAMOWITE ODKRYCIE BRYTYJSKICH NAUKOWCÓW!</div>,
       content: (
         <div>
-          Jeden z{' '}
-          <Highlight
-            type="critical"
-            hint="Tytuł przeczy treści artykułu."
-            visible={visible}>
-            mieszkańców
-          </Highlight>{' '}
-          Londynu przez ponad połowę swojego życia spożywał liście tytoniu.
-          Dożył aż 103 lat. Możemy{' '}
+          Jeden z Londynu przez ponad połowę swojego życia spożywał liście
+          tytoniu. Dożył aż 103 lat. Możemy{' '}
           <Highlight
             type="critical"
             hint="Przedstawiona treść jest tak zwanym dowodem anegdotycznym –
@@ -106,79 +99,141 @@ export const postsByVisibility = visible => [
   },
   {
     fake: {
-      heading: <div>NIESAMOWITE ODKRYCIE BRYTYJSKICH NAUKOWCÓW!</div>,
+      heading: <div>Wpływ długości oglądania telewizji na inteligencje</div>,
       content: (
         <div>
-          Jeden z{' '}
           <Highlight
             type="critical"
-            hint="Tytuł przeczy treści artykułu."
+            hint="Czym jest istotność z punktu widzenia statystyki?"
             visible={visible}>
-            mieszkańców
+            Istotne badania
           </Highlight>{' '}
-          Londynu przez ponad połowę swojego życia spożywał liście tytoniu.
-          Dożył aż 103 lat. Możemy informacji zostały przekupione w wyniku
-          istniejącego{' '}
+          przeprowadzone zostały na{' '}
           <Highlight
             type="critical"
-            hint="Podstawowy błąd atrybucji – błąd w postrzeganiu społecznym
-          polegający na tendencji do wyjaśniania zachowań innych osób w oparciu
-          o cechy charakteru i dotychczas podjęte decyzje przy jednoczesnym
-          braku uwzględnienia wpływu czynników zewnętrznych i roli przypadku."
+            hint="Wiele firm zatrudnia naukowców by przeprowadzali badania i
+            publikowali ich wyniki. "
             visible={visible}>
-            spisku
-          </Highlight>.
+            zlecenie firmy Stramsung (producenta urządzeń multimedialnych)
+          </Highlight>{' '}
+          przez badaczy Krzeczyńsky’ego, Ygrecki’ego, Spasss’a (2018). Za pomocą
+          metody korelacyjnej na grupie{' '}
+          <Highlight
+            type="critical"
+            hint="Im większa liczebność grupy badawczej, tym większa trafność
+            zewnętrzna (możliwość uogólniania uzyskanych wyników na szerszą
+            populację)."
+            visible={visible}>
+            10 dorosłych mężczyzn
+          </Highlight>{' '}
+          udowodnili oni
+          <Highlight
+            type="critical"
+            hint="Badanie korelacyjne dotyczy współwystępowania, a nie wpływu.
+            Bardzo często mylimy te dwa pojęcia, pomimo braku ich tożsamości
+            znaczeniowej. Aby móc wnioskować o przyczynie i skutku badacze
+            mogliby przeprowadzić np. badanie eksperymentalne, w którym
+            manipulowaliby grupami."
+            visible={visible}>
+            wpływ
+          </Highlight>{' '}
+          długości oglądania telewizji na inteligencję. Wyniki badania pokazały,
+          iż im wyższy jest poziom inteligencji u człowieka, tym więcej czasu
+          poświęca on na oglądanie telewizji. W odpowiedzi na badanie środowisko
+          naukowe zasugerowało jego powtórzenie na większej próbie. Jak donosi
+          Trimes, naukowcy{' '}
+          <Highlight
+            type="critical"
+            hint="Badanie powinno zostać zreplikowane (powtórzone) przez
+            niezależnego badacza i uwzględniać, o ile to możliwe, szeroką gamę
+            zróżnicowanych warunków. Ma to na celu potwierdzenie spójności
+            uzyskanych wyników. Pamiętaj: niezwykłe wnioski potrzebują
+            niezwykłych dowodów!"
+            visible={visible}>
+            nie udostępnili procedury badawczej
+          </Highlight>, twierdząc, iż przytoczone przez nich statystyki
+          potwierdziły założone hipotezy.
           <br />
           <br />
-          Żródło: precelek.pl
+          Źródło: Krzeczyńsky, R., Ygrecki A. , Spasss C. (2018). Związek
+          pomiędzy inteligencją a długością oglądania telewizji [on-line].
+          Uzyskane 17.02.2008: www.stramsung.com/
+          <Highlight
+            type="critical"
+            hint="Czy zwróciłeś uwagę na to, że podane źródło wpisu może mieć
+            charakter reklamy?"
+            visible={visible}>
+            dbamy-o-twoja-inteligencje
+          </Highlight>
         </div>
       ),
     },
     legit: {
-      heading: (
-        <div>
-          UMIEJĘTNOŚĆ WYKRYWANIA KŁAMSTWA WŚRÓD POLICJANTÓW I PSYCHOLOGÓW
-        </div>
-      ),
+      heading: <div>Emocje wypisane na twarzy!</div>,
       content: (
         <div>
-          Głównym celem przeprowadzonego procesu badawczego
+          Pomimo wielu badań na temat rozpoznawania emocji na podstawie analizy
+          mimiki, naukowcy
           <Highlight
             type="ok"
-            hint="Powołanie się na pierwotne źródło informacji które zawiera
-          oryginalny opis badań, obserwacji, spostrzeżeń, teorii, hipotez
-          własnych autora"
+            hint="Nauka z definicji opiera się na stawianiu hipotez i
+            poszukiwaniu nowych rozwiązań. Tym samym raz podana informacja może
+            zostać uznana za nieaktualną w wyniku nowych odkryć."
             visible={visible}>
-            (Widacki, Mirska, Wrońska, 2012)
-          </Highlight>, było sprawdzenie, jak psycholodzy oraz policjanci z
-          pionów śledczych, oceniają diagnostyczność symptomów kłamstwa,
-          poruszonych w literaturze przedmiotu. Ponadto zespół badawczy
-          postanowił sprawdzić, czy płeć oraz doświadczenie zawodowe mają wpływ,
-          na wybór tychże symptomów.
+            wciąż nie są zgodni
+          </Highlight>{' '}
+          co do tego, w jakim wieku zdobywamy umiejętność odczytywania emocji w
+          oparciu o obserwację wyrazu twarzy.
+          <br />
+          Aby to sprawdzić zespół badaczy{' '}
           <Highlight
             type="ok"
-            hint="Opis grupy badawczej – dzięki niemu wiemy, że wysnute wnioski
-            dotyczą konkretnej grupy osób, a nie ogółu społeczeństwa."
+            hint="Bardzo dobry trop – w tym artkule zostało podlinkowane
+            oryginalne źródło, tak, aby czytelnik mógł więcej o nim przeczytać."
             visible={visible}>
-            Grupę badawczą stanowiło 201 osób: 100 policjantów (40 kobiet i 60
-            mężczyzn) w średnim wieku 37,4 lat oraz 101 psychologów, (72 kobiet
-            i 29 mężczyzn)
-          </Highlight>, w średnim wieku 28 lat, którzy według badaczy, ze
-          względu na rodzaj świadczonej pracy, zmuszeni są dokonywać oceny
-          wiarygodności rozmowy czy przesłuchiwanego.
+            pod przewodnictwem{' '}
+            <a
+              href="https://www.essex.ac.uk/people/ROBER38300/deborah-roberson"
+              target="_blank"
+              rel="noopener noreferrer">
+              Debi Roberson
+            </a>
+          </Highlight>{' '}
+          przeprowadził trzy eksperymenty z udziałem osób w zróżnicowanym wieku.
+          W badaniach{' '}
+          <Highlight
+            type="ok"
+            hint="Zwróć uwagę na słowo manipulacja – nie zawsze ma ono negatywne
+            znaczenie. W tym przypadku odnosi się ono do metody prowadzonego
+            badania, pozwalając nam na zmianę wybranego czynnika i obserwację
+            wpływu tej zmiany na otrzymywane rezultaty. "
+            visible={visible}>
+            manipulowano dostępnością informacji
+          </Highlight>{' '}
+          pochodzących z różnych części twarzy. W jednym z nich udział wzięło
+          160 osób, po 40 w każdej grupie wiekowej: 3-4 lata, 5-6 lat, 7-8 lat,
+          9-10 lat i 40 osób dorosłych. Zadaniem uczestników eksperymentu było
+          pogrupowanie zdjęć twarzy zgodnie z kryterium pięciu możliwych do
+          wybrania emocji. Na niektórych zdjęciach osoby przedstawione były w
+          okularach. Zgodnie z uzyskanymi wynikami 3-4 latki lepiej radziły
+          sobie z rozpoznawaniem emocji u osób noszących okulary, niż u osób
+          przedstawionych bez nich. U dzieci w wieku 5-8 lat nie zaobserwowano
+          takiej zależności, natomiast u 9-10 latków i dorosłych okulary
+          obniżyły zdolność rozpoznawania emocji.
           <br />
           <br />
+          Źródło: Roberson, D., Kikutani, M., Döge, P., Whitaker, L. & Majid, A.
+          (2012) When less is more: what the addition of sunglasses or masks to
+          faces reveals about the development of facial expression processing.{' '}
           <Highlight
             type="ok"
-            hint="Odwołanie się w bibliografii do konkretnych pozycji naukowych do
-          których odwoływano się w tekście. Opis bibliograficzny powinien
-          zawierać elementy takie jak: autor, tytuł, wydawnictwo, czas i miejsce
-          wydania."
+            hint="Jako źródło informacji podano czasopismo naukowe (przyjmujące
+            artykuły po dokonaniu podwójnie ślepej recenzji, posiadające w
+            gronie recenzentów pracowników naukowych o udokumentowanym dorobku
+            etc.)."
             visible={visible}>
-            Bibliografia
-          </Highlight>: Widacki,J. Mirska,N. Wrońska,M. (2012). Werbalne i
-          niewerbalne symptomy kłamstwa w oczach policjantów oraz psychologów,
-          Przegląd Bezpieczeństwa Wewnętrznego, 7, 19-30.
+            Cognition
+          </Highlight>, 125,195-206.
         </div>
       ),
     },
